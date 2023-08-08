57b4edd1860c0f76f11429218bde8e2f6563a026
silver <ekdldkaa@naver.com>
2023-08-08 10:38:15 +0900

    Add 헤더 로그인여부에 따른 버튼 변화

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import MyPage from "./pages/MyPage";
@@ -11,8 +12,8 @@ import Button from "./components/Button";
 function App() {
   return (
     <BrowserRouter>
-      <Header />
       <div className="App">
+        <Header />
         <Routes>
           <Route path="/goods" element={<Home />} />
           <Route path="/users/:page_type" element={<Users />} />
