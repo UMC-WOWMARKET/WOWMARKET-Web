f5ec914b12020a0b4ad08129635a4d1cd7988c9a
김지원 <jibom0507@naver.com>
2023-08-16 00:10:33 +0900

    axios 일부 수정

--- src/pages/MyOrder.js
+++ src/pages/MyOrder.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavigationBar from "../components/MyPage/NavigationBar";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const MyOrder = () => {
+  const [orderList, setOrderList] = useState([]); // 주문 데이터를 저장할 상태
+  useEffect(() => {
+    axios.interceptors.request.use((config) => {
+      const userAccessToken = localStorage.getItem("accessToken");
+      if (userAccessToken) {
+        console.log(userAccessToken);
+        config.headers["X-ACCESS-TOKEN"] = `${userAccessToken}`;
+      }
+      return config;
+    });
+
+    axios
+      .get("https://www.wowmkt.kr/wowmarket/mypage/myorder?page=${page}")
+      .then((res) => {
+        const responseData = res.data;
+        setOrderList(responseData.orderList);
+      })
+      .catch((err) => {
+        alert("나의 주문폼 로딩 실패!");
+      });
+  }, []);
   return (
     <div className="MyOrder">
       <NavigationBar />
@@ -14,12 +37,26 @@ const MyOrder = () => {
             <div className="third">제출일</div>
             <div className="last">비고</div>
           </div>
-          <div className="content">
-            <div className="order_num">1</div>
-            <div className="order_title">ㅇㅇ 대학교 와움이 인형 세트</div>
-            <div className="order_date">2023-08-11</div>
-            <div className="order_memo">확정대기</div>
-          </div>
+          {orderList.map((order, index) => (
+            <div className="content" key={order.orderid}>
+              <div className="order_num">{index + 1}</div>
+              <div className="order_title">{order.name}</div>
+              <div className="order_date">{order.createdtime}</div>
+              <div className="order_memo">
+                {order.is_del
+                  ? "취소완료"
+                  : order.status === 0
+                  ? "확정대기"
+                  : order.status === 1
+                  ? "주문확정"
+                  : order.status === 2
+                  ? "배송중"
+                  : order.status === 3
+                  ? "배송완료"
+                  : ""}
+              </div>
+            </div>
+          ))}
         </div>
         <div className="footer">
           <div> 프로젝트 제목을 클릭하면 주문폼 내용을 확인할 수 있습니다.</div>
